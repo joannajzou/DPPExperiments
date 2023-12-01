09abfc8e5cb3e470372299ca23d421db5ef3fb76
joannajzou <joannajzou@gmail.com>
2023-12-01 15:40:23 -0500

    functions for reading POD descriptors

--- experiments/read_pod_descr.jl
+++
@@ -1,9 +0,0 @@
-push!(Base.LOAD_PATH, "../../")
-
-using PotentialLearning
-using LinearAlgebra, Random, Statistics, StatsBase, Distributions
-using AtomsBase, Unitful, UnitfulAtomic
-using InteratomicPotentials, InteratomicBasisPotentials
-using Determinantal
-using DataFrames
-using JLD
